5ecd3d8a4618c26a71fa999f37a2593f61e6694c
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 20:25:56 +0500

    basic32.js: функция matrixMultiplication умножает две матрицы

--- src/basic/basic32.js
+++ src/basic/basic32.js
@@ -1,7 +1,29 @@
 /* 
 	Напишите функцию matrixMultiplication(A, B), выполняющую перемножение матриц А и В.
 */
-
+// Функция умножает две матрицы
 export function matrixMultiplication(A, B) {
-  return [[1]];
+  if (A.length === 0 || B.length === 0) return [[]]
+  let rowsA = A.length
+  let colsA = A[0].length
+  let colsB = B[0].length
+  let result = []
+  let i = 0
+  while (i < rowsA) {
+    let row = []
+    let j = 0
+    while (j < colsB) {
+      let sum = 0
+      let k = 0
+      while (k < colsA) {
+        sum += A[i][k] * B[k][j]
+        k++
+      }
+      row.push(sum)
+      j++
+    }
+    result.push(row)
+    i++
+  }
+  return result
 }
