73327e7a7e9660eecb73defa7d4e42cbbd72f1a1
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 15:38:48 +0500

    basic18.js: функция second_largest находит второе максимальное значение через цикл while

--- src/basic/basic18.js
+++ src/basic/basic18.js
@@ -1,8 +1,21 @@
 /* 
 	Напишите функцию second_largest(arr), находящую второе максимальное значение в массиве. Если такого элемента нет - вернуть null. Функцию сортировки использовать нельзя, можно использовать циклы while или for.
 */
-
+// Функция находит второе максимальное значение через цикл while
 export function second_largest(arr) {
-  return null;
+    if (arr.length < 2) return null
+    let max = arr[0]
+    let second = null
+    let i = 1
+    while (i < arr.length) {
+        if (arr[i] > max) {
+            second = max
+            max = arr[i]
+        } else if (arr[i] < max && (second === null || arr[i] > second)) {
+            second = arr[i]
+        }
+        i++
+    }
+    return second
 }
 
