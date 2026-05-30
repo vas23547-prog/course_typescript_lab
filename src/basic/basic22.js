4cd679e1037c7c77a8490adda0e181edb88a6a0e
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 16:51:25 +0500

    basic22.js: функция palindrome_check проверяет палиндром через reverse

--- src/basic/basic22.js
+++ src/basic/basic22.js
@@ -2,7 +2,8 @@
 	Реализуйте функцию palindrome_check(word), которая проверяет, является ли слово палиндромом (читается одинаково слева направо и справа налево).
 	Подсказка: используйте методы split и join для строк и метод reverse для массивов.
 */
-
+// Функция проверяет палиндром через сравнение с обратной строкой
 export function palindrome_check(word) {
-	return true;
+  let reversed = word.split('').reverse().join('')
+  return word === reversed
 }
