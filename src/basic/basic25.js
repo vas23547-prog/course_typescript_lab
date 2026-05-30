537ef880fe2ee48f0baf62a49a5851cc48870a13
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 17:45:35 +0500

    basic25.js: функция removeFirstOccurrence удаляет первое вхождение элемента в массиве

--- src/basic/basic25.js
+++ src/basic/basic25.js
@@ -1,7 +1,18 @@
 /* 
 	Реализуйте функцию removeFirstOccurrence(arr, value), которая удаляет первое вхождение указанного элемента из массива
 */
-
+// Функция удаляет первое вхождение элемента через цикл while
 export function removeFirstOccurrence (arr, value) {
-  return arr
+  let result = []
+  let found = false
+  let i = 0
+  while (i < arr.length) {
+    if (arr[i] === value && !found) {
+      found = true
+    } else {
+      result.push(arr[i])
+    }
+    i++
+  }
+  return result
 }
