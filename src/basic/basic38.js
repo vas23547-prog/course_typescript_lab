0072a471c2209f366e0d0df4c4b57a672d8c7407
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 22:09:06 +0500

    basic38.js: функция some проверяет выполнение условия хотя бы для одного элемента

--- src/basic/basic38.js
+++ src/basic/basic38.js
@@ -1,9 +1,15 @@
 /* 
 	Реализовать функцию some, которая принимает массив и функцию-callback. Callback принимает один элемент массива и возвращает true или false. Функция some должна вернуть true, если хотя бы один элемент массива вернул true в callback. Нельзя использовать метод массивов some.
 */
-
 export function some(arr, callback) {
-	return false;
+  let i = 0
+  while (i < arr.length) {
+    if (callback(arr[i])) {
+      return true
+    }
+    i++
+  }
+  return false
 }
 
 
