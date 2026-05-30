16dd4e3db3decccd539b9a1d4e1daf075cd58cae
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 18:46:50 +0500

    basic28.js: функция removeDuplicates удаляет дубликаты через indexOf и push

--- src/basic/basic28.js
+++ src/basic/basic28.js
@@ -1,8 +1,15 @@
 /* 
 	Реализуйте функцию removeDuplicates(arr), которая удаляет повторяющиеся элементы из массива с помощью методов indexOf и push.
 */
-
+// Функция удаляет дубликаты через indexOf и push
 export function removeDuplicates(arr) {
-
-	return arr;
+  let result = []
+  let i = 0
+  while (i < arr.length) {
+    if (result.indexOf(arr[i]) === -1) {
+      result.push(arr[i])
+    }
+    i++
+  }
+  return result
 }
