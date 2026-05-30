0d869ff95068605da19109253f8f09d042e31789
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 14:19:10 +0500

    basic11.js: функция is_prime проверяет простоту числа через цикл while

--- src/basic/basic11.js
+++ src/basic/basic11.js
@@ -1,9 +1,16 @@
 /* 
 	Реализуйте функцию is_prime(n), которая проверяет, является ли число n простым. Простое число — это натуральное число больше единицы, которое делится только на 1 и само себя. Функция должна использовать цикл while.
 */
-
+// Функция проверяет простоту числа через цикл while
 export function is_prime(n) {
-	return true;
+  if (n <= 1) return false
+  if (n === 2) return true
+  let i = 2
+  while (i <= Math.sqrt(n)) {
+    if (n % i === 0) return false
+    i++
+  }
+  return true
 }
 
 
