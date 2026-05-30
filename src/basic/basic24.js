57e036fb38ea3616b9c366b932002eea38a76b7b
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 17:28:05 +0500

    basic24.js: функция replace_word заменяет слово в предложении через цикл while

--- src/basic/basic24.js
+++ src/basic/basic24.js
@@ -1,8 +1,16 @@
 /* 
 	Напишите функцию replace_word(sentence, old_word, new_word), заменяющую старое слово новым словом в предложении.
 */
-
+// Функция заменяет слово в предложении через split и join
 export function replace_word(sentence, old_word, new_word) {
-    return "";
+    let words = sentence.split(' ')
+    let i = 0
+    while (i < words.length) {
+        if (words[i] === old_word) {
+            words[i] = new_word
+        }
+        i++
+    }
+    return words.join(' ')
 }
 
