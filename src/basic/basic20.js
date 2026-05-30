8b651cda4ae6939e0dc2c7e2cbeb774836398653
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 16:04:39 +0500

    basic20.js: функция concatenate_strings объединяет строки через цикл while

--- src/basic/basic20.js
+++ src/basic/basic20.js
@@ -1,7 +1,13 @@
 /* 
 	Напишите функцию concatenate_strings(arr), объединяющую строки из массива в одну длинную строку. Функция не должна использовать функции высших порядков и оператор ..., можно использовать циклы while или for.
 */
-
+// Функция объединяет строки из массива через цикл while
 export function concatenate_strings(arr) {
-	return "";
+  let result = ""
+  let i = 0
+  while (i < arr.length) {
+    result += arr[i]
+    i++
+  }
+  return result
 }
