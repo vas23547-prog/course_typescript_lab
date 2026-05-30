63e0d1c9ec00dd2a4c891142f7c71a96afbd4e54
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 14:40:49 +0500

    basic13.js: функция average вычисляет среднее через цикл while

--- src/basic/basic13.js
+++ src/basic/basic13.js
@@ -1,8 +1,14 @@
 /* 
 	Напишите функцию average(arr), которая рассчитывает среднее арифметическое элементов массива. Функция не должна использовать функции высших порядков, можно использовать циклы while или for.
 */
-
+// Функция вычисляет среднее арифметическое через цикл while
 export function average(arr) {
-	return 0
+  let sum = 0
+  let i = 0
+  while (i < arr.length) {
+    sum += arr[i]
+    i++
+  }
+  return arr.length > 0 ? sum / arr.length : 0
 }
 
