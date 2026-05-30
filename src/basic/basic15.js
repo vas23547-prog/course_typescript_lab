5d2cde9cb03030277dd1880d33a2bd5edaba418b
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 15:05:31 +0500

    basic15.js: функция swap_first_last возвращает новый массив с переставленными первым и последним элементами

--- src/basic/basic15.js
+++ src/basic/basic15.js
@@ -1,7 +1,18 @@
 /* 
 	Напишите функцию swap_first_last(arr), меняющую местами первый и последний элемент массива. Функция не должна использовать функции высших порядков и оператор ..., также она не должна изменять исходный массив, можно использовать циклы while или for.
 */
-
+// Функция возвращает новый массив с переставленными первым и последним элементами
 export function swap_first_last(arr) {
-    return arr;
+    let result = []
+    let i = 0
+    while (i < arr.length) {
+        result.push(arr[i])
+        i++
+    }
+    if (result.length > 0) {
+        let temp = result[0]
+        result[0] = result[result.length - 1]
+        result[result.length - 1] = temp
+    }
+    return result
 }
