9ad5fc21f21b9b9d5c276dc436c5864ea80b97e9
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 15:11:50 +0500

    basic16.js: функция reverse_list возвращает массив в обратном порядке через цикл while

--- src/basic/basic16.js
+++ src/basic/basic16.js
@@ -1,8 +1,14 @@
 /* 
 	Реализуйте функцию reverse_list(arr), которая переворачивает порядок элементов массива. Функция не должна использовать функции высших порядков, можно использовать циклы while или for.
 */
-
+// Функция возвращает массив в обратном порядке через цикл while
 export function reverse_list(arr) {
-    return arr;
+    let result = []
+    let i = arr.length - 1
+    while (i >= 0) {
+        result.push(arr[i])
+        i--
+    }
+    return result
 }
 
