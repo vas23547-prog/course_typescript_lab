aca16914fa822d1f13439945d0adba85cf6c5421
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 20:05:44 +0500

    basic31.js: функция transposeMatrix транспонирует матрицу через циклы while

--- src/basic/basic31.js
+++ src/basic/basic31.js
@@ -1,7 +1,22 @@
 /* 
 	Реализуйте функцию transposeMatrix(matrix), которая транспонирует матрицу (меняет строки и столбцы местами).
 */
-
-export function transposeMatrix(matrix) {	
-	return [[]];
+// Функция транспонирует матрицу через циклы while
+export function transposeMatrix(matrix) {
+  if (matrix.length === 0) return [[]]
+  let rows = matrix.length
+  let cols = matrix[0].length
+  let result = []
+  let j = 0
+  while (j < cols) {
+    let newRow = []
+    let i = 0
+    while (i < rows) {
+      newRow.push(matrix[i][j])
+      i++
+    }
+    result.push(newRow)
+    j++
+  }
+  return result
 }
