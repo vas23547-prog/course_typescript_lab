08352c9090188c685aa482f2398d5f97236cefde
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 18:13:04 +0500

    basic27.js: функция insertAtPosition вставляет элемент через slice и concat

--- src/basic/basic27.js
+++ src/basic/basic27.js
@@ -1,7 +1,7 @@
 /* 
 	Реализуйте функцию insertAtPosition(arr, pos, elem), которая вставляет новый элемент в указанную позицию массива с помощью методов slice и concat.
 */
-
+// Функция вставляет элемент в позицию через slice и concat
 export function insertAtPosition(arr, pos, elem) {
-  return arr;
+  return arr.slice(0, pos).concat([elem], arr.slice(pos))
 }
