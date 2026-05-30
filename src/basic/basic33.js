f8b441257757a9aaf897a665d2d1620a5f99ce87
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 20:47:46 +0500

    basic33.js: функция zeroRowColumn обнуляет строку и столбец в матрице

--- src/basic/basic33.js
+++ src/basic/basic33.js
@@ -1,7 +1,17 @@
 /* 
 	Реализуйте функцию zeroRowColumn(matrix, row, col), которая превращает в ноль всю строку и весь столбец матрицы, соответствующие указанным индексам.
 */
-
+// Функция обнуляет строку и столбец в матрице
 export function zeroRowColumn(matrix, row, col) {
-    return matrix;
+  let i = 0
+  while (i < matrix.length) {
+    matrix[i][col] = 0
+    i++
+  }
+  let j = 0
+  while (j < matrix[row].length) {
+    matrix[row][j] = 0
+    j++
+  }
+  return matrix
 }
