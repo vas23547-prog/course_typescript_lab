5d6beb8c93b6abc0eb79fa03ff8d2bab7d15e8e9
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 15:56:46 +0500

    basic19.js: функция sum_positive складывает положительные элементы через цикл while

--- src/basic/basic19.js
+++ src/basic/basic19.js
@@ -1,9 +1,17 @@
 /* 
 	Реализуйте функцию sum_positive(arr), которая складывает положительные элементы массива. Функция не должна использовать функции высших порядков, можно использовать циклы while или for.
 */
-
+// Функция складывает положительные элементы через цикл while
 export function sum_positive(arr) {
-	return 0;
+  let sum = 0
+  let i = 0
+  while (i < arr.length) {
+    if (arr[i] > 0) {
+      sum += arr[i]
+    }
+    i++
+  }
+  return sum
 }
 
 
