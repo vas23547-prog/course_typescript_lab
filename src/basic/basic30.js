b2b37ecc64d248d1f9bafbc8a2aa9238bb1e0385
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 19:39:23 +0500

    basic30.js: функция extractUniqueValues добавляет уникальные элементы arr1 в arr2 через indexOf и push

--- src/basic/basic30.js
+++ src/basic/basic30.js
@@ -1,8 +1,15 @@
 /* 
 	РРеализуйте функцию extractUniqueValues(arr1, arr2), которая берет уникальные элементы первого массива и помещает их в конец второго массива с помощью методов indexOf и push.
 */
-
+// Функция добавляет уникальные элементы arr1 в конец arr2 через indexOf и push
 export function extractUniqueValues(arr1, arr2) {
-    return arr1;
+  let i = 0
+  while (i < arr1.length) {
+    if (arr2.indexOf(arr1[i]) === -1) {
+      arr2.push(arr1[i])
+    }
+    i++
+  }
+  return arr2
 }
 
