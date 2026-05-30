febc7c1858cca87b00060e789bd59b803390e336
VasilchenkoEVG <vas23547@gmail.com>
2026-05-30 22:41:01 +0500

    basic39.js: функция all проверяет, что все элементы удовлетворяют callback

--- src/basic/basic39.js
+++ src/basic/basic39.js
@@ -1,9 +1,15 @@
 /* 
 	Реализовать функцию all, которая принимает массив и функцию-callback. Callback принимает один элемент массива и возвращает true или false. Функция all должна вернуть true, если все элементы массива вернули true в callback. Нельзя использовать метод массивов every.
 */
-
+// Функция all проверяет, что все элементы удовлетворяют callback
 export function all(arr, callback) {
-
-	return true;
+  let i = 0
+  while (i < arr.length) {
+    if (!callback(arr[i])) {
+      return false
+    }
+    i++
+  }
+  return true
 }
 
