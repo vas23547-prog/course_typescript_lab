dd2c659a6a547ca3052e0943264e52191c26b04f
VasilchenkoEVG <vas23547@gmail.com>
2026-05-31 10:02:05 +0500

    objects02.ts: метод equal проверяет равенство имени и возраста

--- src/objects/objects02.ts
+++ src/objects/objects02.ts
@@ -1,7 +1,7 @@
-/* 	
+/*
 	В объекте user создать метод equal, который будет проверять равенство объектов. При передачи аргументов метод должен возвращать копию объекта с новыми значениями свойств.
 */
-
+// Метод equal проверяет равенство имени и возраста
 export type User = {
   name: string;
   age: number;
@@ -12,7 +12,7 @@ export const user: User = {
   name: "John",
   age: 30,
   equal(other) {
-	return true;
+	return this.name === other.name && this.age === other.age
   }
 };
 
