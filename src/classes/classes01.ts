5ffdba321c4119b174e6d84e2365c90c20377866
VasilchenkoEVG <vas23547@gmail.com>
2026-05-31 18:56:47 +0500

    classes01.ts: конструктор Person с модификаторами public/private

--- src/classes/classes01.ts
+++ src/classes/classes01.ts
@@ -1,14 +1,16 @@
-/* 
+/*
 	Напишите конструктор класса Person, который принимает три аргумента: имя, фамилию и возраст. Используйте модульность доступа (модификаторы доступа public, private) для соответствующих полей.
 */
 
 export class Person {
-    firstName: string;
-    lastName: string;
+    public firstName: string;
+    public lastName: string;
     private age: number;
 
     constructor(firstName: string, lastName: string, age: number) {
-        
+         this.firstName = firstName
+         this.lastName = lastName
+         this.age = age
     }
 
     getFullName(): string {
