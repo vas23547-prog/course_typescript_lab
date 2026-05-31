8a0ee94c93ceeaf428c1c7f8d434ca7552230285
VasilchenkoEVG <vas23547@gmail.com>
2026-05-31 11:16:30 +0500

    objects01.ts: метод copy возвращает копию объекта с возможными новыми значениями

--- src/objects/objects01.ts
+++ src/objects/objects01.ts
@@ -1,7 +1,7 @@
-/* 	
+/*
 	В объекте user создать метод copy, который будет возвращать копию объекта. При передачи аргументов метод должен возвращать копию объекта с новыми значениями свойств.
 */
-
+// Метод copy возвращает копию объекта с возможными новыми значениями
 export type User = {
   name: string;
   age: number;
@@ -12,6 +12,10 @@ export const user: User = {
   name: "John",
   age: 30,
   copy(name?: string, age?: number) {
-	return this
+	return {
+	  name: name !== undefined ? name : this.name,
+	  age: age !== undefined ? age : this.age,
+	  copy: this.copy
+	}
   },
 };
