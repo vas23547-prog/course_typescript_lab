2bb38dc00c96fbb3a544d58c3162e7f83da449d6
VasilchenkoEVG <vas23547@gmail.com>
2026-05-31 22:46:31 +0500

    classes09.ts: класс Product с методом applyDiscount

--- src/classes/classes09.ts
+++ src/classes/classes09.ts
@@ -1,19 +1,24 @@
-/* 	
+/*
 	В классе Product нужно добавить статическое свойство discountRate, указывающее скидку на товары, и статический метод applyDiscount, применяющий скидку ко всей продукции.
 */
 
 export class Product {
-  
+   static discountRate = 0.1;
+
   name: string;
   price: number;
   constructor(name: string, price: number) {
-    this.name = name;
-    this.price = price;
+	this.name = name;
+	this.price = price;
   }
   describe() {
-    console.log(`Продукт: ${this.name}, цена: ${this.price}`);
-  } 
+	console.log(`Продукт: ${this.name}, цена: ${this.price}`);
+  }
   static applyDiscount(products: Product[]) {
-  
+	let i = 0
+	while (i < products.length) {
+	  products[i].price = products[i].price * (1 - Product.discountRate)
+	  i++
+	}
   }
 }
