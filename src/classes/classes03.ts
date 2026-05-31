6396d21eb2ec3014665b31320ca109eefa602682
VasilchenkoEVG <vas23547@gmail.com>
2026-05-31 21:07:24 +0500

    classes03.ts: конструктор Student создает объект Person и группу

--- src/classes/classes03.ts
+++ src/classes/classes03.ts
@@ -1,4 +1,4 @@
-/* 	
+/*
 	Реализовать конструктор класса Student, который принимает имя, возраст и группу.
 */
 
@@ -6,8 +6,8 @@ class Person {
   name: string;
   age: number;
   constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
+	this.name = name;
+	this.age = age;
   }
 }
 
@@ -15,6 +15,7 @@ export class Student {
 	person: Person;
 	group: string;
 	constructor(name: string, age: number, group: string) {
-
+	  this.person = new Person(name, age)
+	  this.group = group
 	}
 }
