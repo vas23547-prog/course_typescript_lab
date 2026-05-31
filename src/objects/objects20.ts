801788f50db0eaf115592f81f46d66858d0b4d53
VasilchenkoEVG <vas23547@gmail.com>
2026-05-31 16:02:58 +0500

    objects20.ts: функция fromArrays собирает объект из массивов ключей и значений

--- src/objects/objects20.ts
+++ src/objects/objects20.ts
@@ -1,9 +1,14 @@
-/* 
+/*
 	Создайте функцию, которая собирает объект из двух массивов: массива ключей и массива значений.
 */
 
 export function fromArrays<T, K extends string>(keys: K[], values: T[]): Record<K, T> {
-	const result: any = {};
-	return result;
+  const result: any = {}
+  let i = 0
+  while (i < keys.length && i < values.length) {
+	result[keys[i]] = values[i]
+	i++
+  }
+  return result
 }
 
