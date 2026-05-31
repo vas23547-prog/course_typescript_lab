941e2f476cf0ff8416e65611746955a43a165b18
VasilchenkoEVG <vas23547@gmail.com>
2026-05-31 22:36:29 +0500

    classes08.ts: класс Animal с подсчетом животных через animalCount

--- src/classes/classes08.ts
+++ src/classes/classes08.ts
@@ -1,23 +1,24 @@
-/*   
+/*
   Дан исходный класса Animal. Необходимо добавить статическое свойство animalCount, которое учитывает количество созданных животных, и статический метод getAnimalCount, возвращающий это значение.
 */
 
 export class Animal {
-  
+   static animalCount = 0;
+
   name: string;
   species: string;
   constructor(name: string, species: string) {
-    this.name = name;
-    this.species = species;
-    
+	this.name = name;
+	this.species = species;
+	Animal.animalCount++
   }
   greet() {
-    console.log(
-      `Привет, меня зовут ${this.name}! Я представитель вида ${this.species}.`
-    );
+	console.log(
+	  `Привет, меня зовут ${this.name}! Я представитель вида ${this.species}.`
+	);
   }
   static getAnimalCount() {
-    
+	return Animal.animalCount
   }
 }
 
