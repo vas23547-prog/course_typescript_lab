dab8f17ce601eedd8955b0e5185a9b78b98a4e48
VasilchenkoEVG <vas23547@gmail.com>
2026-05-31 12:39:35 +0500

    objects08.ts: функция sum суммирует элементы через метод sum

--- src/objects/objects08.ts
+++ src/objects/objects08.ts
@@ -7,5 +7,11 @@ export type Summable = {
 }
 
 export function sum<T extends Summable>(a: T[]): T {
-	return a[0];
+	let result = a[0]
+	let i = 1
+	while (i < a.length) {
+		result = result.sum(a[i] as Summable) as T
+		i++
+	}
+	return result
 }
