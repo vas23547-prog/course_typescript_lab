104bd36bb4d4906646781bf0c2fe2ec4c11c035f
VasilchenkoEVG <vas23547@gmail.com>
2026-05-31 14:10:05 +0500

    objects12.ts: исправлено использование метода-функции вместо стрелочной функции

--- src/objects/objects12.ts
+++ src/objects/objects12.ts
@@ -1,12 +1,12 @@
-/* 
+/*
 	Функция add должна добавлять переданное значение к предыдущему значению, но она работает некорректно. Почему? Исправьте ошибку.
 */
 
 const calculator = {
-    previousValue: 0,
-    add: (value: number) => {
-        return this.previousValue += value;
-    }
+  previousValue: 0,
+  add(value: number) {
+	return this.previousValue += value;
+  }
 };
 
 export function add(value: number) {
