35cd6090f8b2bebcf58d8c669880111356c68954
VasilchenkoEVG <vas23547@gmail.com>
2026-05-31 21:44:01 +0500

    classes05.ts: класс Task с перегруженными конструкторами

--- src/classes/classes05.ts
+++ src/classes/classes05.ts
@@ -1,14 +1,14 @@
-/* 
+/*
 	Класс Task, позволяющий создать задачу либо простым текстом («Купить хлеб»), либо путем передачи названия и статуса.
 */
-
 
 export class Task {
     title: string;
     done: boolean;
-    constructor(title: string);                  // Только название задачи
-    constructor(title: string, done: boolean);   // Название и статус завершения
+    constructor(title: string);
+    constructor(title: string, done: boolean);
     constructor(title: string, done?: boolean) {
-
+	this.title = title
+	this.done = done !== undefined ? done : false
     }
 }
