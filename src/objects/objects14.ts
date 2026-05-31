702614771b1071f9a565fa69626194d2381f7c47
VasilchenkoEVG <vas23547@gmail.com>
2026-05-31 14:26:58 +0500

    objects14.ts: заменена функция на стрелочную для доступа к this.value

--- src/objects/objects14.ts
+++ src/objects/objects14.ts
@@ -1,12 +1,12 @@
-/* 
+/*
 	Метод getInnerFunction объекта objectWithInnerFunction должен возвращать функцию, которая возвращает значение свойства value. Однако он работает некорректно. Почему? Исправьте ошибку.
 */
 
 export const objectWithInnerFunction = {
-    value: 10,
-    getInnerFunction() {
-        return function() {
-            return this.value; 
-        };
-    }
+  value: 10,
+  getInnerFunction() {
+	return () => {
+	  return this.value;
+	};
+  }
 };
