d153ec8623bd43b414c5970bd22e79c7e0882200
VasilchenkoEVG <vas23547@gmail.com>
2026-05-31 21:27:53 +0500

    classes04.ts: класс Point с перегруженными конструкторами для разных аргументов

--- src/classes/classes04.ts
+++ src/classes/classes04.ts
@@ -1,12 +1,16 @@
-/* 
+/*
 	Создать класс Point, который принимает координаты точки либо отдельно (по оси X и Y), либо сразу массивом координат [X,Y,Z].
 */
 
 export class Point {
     coords: number[];
-    constructor(x: number, y: number);   // Конструктор с двумя числами
-    constructor(coords: [number, number]);       // Конструктор с массивом чисел
+    constructor(x: number, y: number);
+    constructor(coords: [number, number]);
     constructor(...args: any[]) {
-		
+	if (args.length === 1 && Array.isArray(args[0])) {
+	  this.coords = args[0]
+	} else {
+	  this.coords = [args[0], args[1]]
+}
     }
 }
