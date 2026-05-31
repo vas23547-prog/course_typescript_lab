19d3f14559c6c6d7b51249c44548a01928e11b22
VasilchenkoEVG <vas23547@gmail.com>
2026-05-31 15:25:56 +0500

    objects18.ts: метод uppercaseProfileNames возвращает объект с  name в верхнем регистре

--- src/objects/objects18.ts
+++ src/objects/objects18.ts
@@ -1,10 +1,13 @@
-/* 
+/*
 	Реализовать метод uppercaseProfileNames в объекте user, который возвращает новый объект, такого же типа как и исходный, но со значением name в верхнем регистре.
 */
 
 export const user = {
-    profile: { name: 'Alice', age: 30 },
-    uppercaseProfileNames() {
-        return this;
-    }
+  profile: { name: 'Alice', age: 30 },
+  uppercaseProfileNames() {
+	return {
+	  profile: { name: this.profile.name.toUpperCase(), age: this.profile.age },
+	  uppercaseProfileNames: this.uppercaseProfileNames
+	}
+  }
 };
