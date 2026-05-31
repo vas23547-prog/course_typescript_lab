5f68ffe48ab16dadd0ddc080f7a102375220b5ef
VasilchenkoEVG <vas23547@gmail.com>
2026-05-31 15:38:13 +0500

    objects19.ts: метод withTaxRate добавляет taxAmount через умножение price на rate

--- src/objects/objects19.ts
+++ src/objects/objects19.ts
@@ -1,10 +1,14 @@
-/* 
+/*
 	Реализовать метод withTaxRate в объекте product, который возвращает новый объект, такого же типа как и исходный, но с добавленным полем taxAmount, равным произведению price и taxRate.
 */
 
 export const product = {
-    price: 100,
-    withTaxRate(rate: number) {
-        return this;
-    }
+  price: 100,
+  withTaxRate(rate: number) {
+	return {
+	  price: this.price,
+	  withTaxRate: this.withTaxRate,
+	  taxAmount: this.price * rate
+	}
+  }
 };
