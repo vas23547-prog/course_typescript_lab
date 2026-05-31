55584b931b9f9bb115f999d30f1338cc16d9d3f8
VasilchenkoEVG <vas23547@gmail.com>
2026-05-31 14:39:03 +0500

    objects15.ts: функция updateField обновляет поле объекта через spread

--- src/objects/objects15.ts
+++ src/objects/objects15.ts
@@ -1,9 +1,8 @@
-/* 
+/*
 	Реализовать функцию updateField, которая принимает объект и ключ (имя поля) и возвращает новый объект с измененным значением поля. Если поля с таким именем нет, то его нужно добавить.
 */
 
-
 export function updateField<T, K extends keyof T>(obj: T, field: K, value: T[K]): T {
-	return obj;
+  return { ...obj, [field]: value }
 }
 
