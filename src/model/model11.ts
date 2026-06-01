43dc7f0e693b022a31809e4d65a78b507e0bba2f
VasilchenkoEVG <vas23547@gmail.com>
2026-06-01 23:48:44 +0500

    model11.ts: реализованы методы save и load для хранения данных в памяти

--- src/model/model11.ts
+++ src/model/model11.ts
@@ -11,4 +11,10 @@ abstract class DataStorage {
 export class MemoryStorage extends DataStorage {
   private data: string = "";
   
+  save(data: string): void {
+	  this.data = data
+  }
+  load(): string {
+	  return this.data
+  }
 }
