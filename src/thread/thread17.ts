337da45f01ef9e66ba5fa0d9caf01213d266c0bf
VasilchenkoEVG <vas23547@gmail.com>
2026-06-03 20:48:41 +0500

    thread17.ts: функция fetchUserWithRetry реализована с механизмом повторных попыток

--- src/thread/thread17.ts
+++ src/thread/thread17.ts
@@ -6,4 +6,19 @@ import { fetchUser, type User } from "./promises";
 
 export function fetchUserWithRetry(id: number, maxAttempts = 3): Promise<User> {
   // TODO: Реализовать retry механизм
+  return new Promise<User>((resolve, reject) => {
+    const attempt = (currentAttempt: number) => {
+      fetchUser(id)
+        .then(resolve)
+        .catch(error => {
+          if (currentAttempt >= maxAttempts) {
+            reject(error);
+          } else {
+            attempt(currentAttempt + 1);
+          }
+        });
+    };
+    
+    attempt(1);
+  });
 }
