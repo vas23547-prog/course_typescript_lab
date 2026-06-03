297c4d81776c5e7ec214ca9440c4786a3f03ae46
VasilchenkoEVG <vas23547@gmail.com>
2026-06-03 21:47:05 +0500

    thread19.ts: функция fetchUserWithTimeout реализована с таймаутом и очисткой таймера

--- src/thread/thread19.ts
+++ src/thread/thread19.ts
@@ -6,5 +6,13 @@ import { fetchUser, type User } from "./promises";
 
 export function fetchUserWithTimeout(id: number, timeout = 100): Promise<User> {
   // TODO: Добавить таймаут для промиса
-
+  let timeoutId: NodeJS.Timeout;
+  const timeoutPromise = new Promise<never>((_, reject) => {
+    timeoutId = setTimeout(() => reject(new Error("Request timeout")), timeout);
+  });
+  
+  return Promise.race([
+    fetchUser(id).finally(() => clearTimeout(timeoutId)),
+    timeoutPromise
+  ]);
 }
