e8e9b49701a822f968ffc043b356cc7db1bdf586
VasilchenkoEVG <vas23547@gmail.com>
2026-06-03 23:12:51 +0500

    thread21.ts: функция parsePositiveNumber реализована с использованием andThen для проверки положительного числа.

--- src/thread/thread21.ts
+++ src/thread/thread21.ts
@@ -2,11 +2,17 @@
 	Доделайте функцию, которая использует andThen для последовательной обработки: парсит число, затем проверяет, что оно положительное. Если число не положительное, то возвращает ошибку "Number must be positive".
 */
 
-import { Result } from 'neverthrow'
+import { Result, err, ok } from 'neverthrow'
 import { parseNumber } from './thread20'
 
 export const parsePositiveNumber = (input: string): Result<number, string> => {
   return parseNumber(input)
-	// Ваш код здесь (3-5 строк)
+	.andThen(num => {
+	  if (num > 0) {
+		return ok(num);
+	  } else {
+		return err("Number must be positive");
+	  }
+	});
 }
 
